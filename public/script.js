0071f2df0ad570affa8da35c096d640aea24a8cf
yalicohen09 <115401088+yalicohen09@users.noreply.github.com>
2025-04-26 23:18:06 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -67,7 +67,7 @@ btcCanvas.height = btcCanvas.offsetHeight;
 let btcData = [];
 
 function drawBtcGraph() {
-  fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd')
+  fetch('/btc-price')
     .then(response => response.json())
     .then(data => {
       const price = data.bitcoin.usd;
